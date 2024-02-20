7efaa819bb76f275c44ab9e39f73da2b96f89e9f
javierlago <javierlagoamoedo@gmail.com>
2024-02-20 12:35:30 +0100

    wp

---
+++ VentanasNotificaciones/Notificaciones/app/src/main/java/com/example/notificaciones/MainActivity.java
@@ -0,0 +1,197 @@
+package com.example.notificaciones;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    String color;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnsVentanas(View view) {
+        switch (view.getId()) {
+            case R.id.dialogo_con_mensaje: {
+                dialogoMensaje();
+                break;
+            }
+            case R.id.ventana2: {
+                dialogoVentana1Boton();
+                break;
+            }
+            case R.id.button3: {
+                dialogoConLista();
+                break;
+            }
+            case R.id.button4: {
+                // Opcion con funcionalidad cuestionable.No se puede hacer asi ya que al seleccionar un item se cierra la ventana sin la posibilidad de darle al boton "OK"
+                dialogoConListaYbotonAcept();
+                break;
+            }
+            case R.id.button5: {
+                dialogoConListaYbotonAceptBien();
+                break;
+            }
+            case R.id.button6: {
+                dialogoConListaMultipleYbotonAceptBien();
+                break;
+            }
+
+
+        }
+
+
+    }
+
+
+    // Metodo con el que se crea la ventana
+    private void dialogoMensaje() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setIcon(R.drawable.taxi);
+        alertDialogBuilder.setMessage("Esto es una ventana");
+        alertDialogBuilder.setTitle("Atencion");
+        alertDialogBuilder.show();
+
+
+    }
+
+    private void dialogoVentana1Boton() {
+        AlertDialog.Builder ventana = new AlertDialog.Builder(this);
+        ventana.setIcon(R.drawable.taxi).setTitle("esto es un aviso").setMessage("hola que tal");
+        ventana.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Toast.makeText(MainActivity.this, "Has pulsado ok", Toast.LENGTH_SHORT).show();
+                // Personalizacion de las toast
+                Toast toast = Toast.makeText(MainActivity.this, "Toast por defecto", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+                dialog.cancel();
+            }
+        });
+        ventana.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Has pulsado cancel", Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+        ventana.show();
+    }
+
+    private void dialogoConLista() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setIcon(R.drawable.taxi);
+        alertDialogBuilder.setTitle("Escoje un color");
+        alertDialogBuilder.setItems(R.array.colores, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Has elegido " + getResources().getStringArray(R.array.colores)[which], Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+        alertDialogBuilder.show();
+    }
+
+    private void dialogoConListaYbotonAcept() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setIcon(R.drawable.taxi);
+        alertDialogBuilder.setTitle("Escoje un color con boton");
+        alertDialogBuilder.setItems(R.array.colores, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                color = getResources().getStringArray(R.array.colores)[which];
+
+            }
+        });
+
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Has elegido " + color, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alertDialogBuilder.show();
+    }
+
+    private void dialogoConListaYbotonAceptBien() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setIcon(R.drawable.taxi);
+        alertDialogBuilder.setTitle("Escoje un color con boton");
+        alertDialogBuilder.setSingleChoiceItems(R.array.colores, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                color = getResources().getStringArray(R.array.colores)[which];
+            }
+        });
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Has elegido " + color, Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertDialogBuilder.show();
+
+
+    }
+
+
+    private void dialogoConListaMultipleYbotonAceptBien() {
+        ArrayList<Integer> selectedItems = new ArrayList<>();  // Where we track the selected items
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setIcon(R.drawable.taxi);
+        alertDialogBuilder.setTitle("Escoje un color con boton");
+        alertDialogBuilder.setMultiChoiceItems(R.array.colores, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked) {
+                    // Verificamos que el item ha sido seleccionado y lo añadimos a un array
+                    selectedItems.add(which);
+
+                } else if (selectedItems.contains(which)) {
+                    // si se vuelve a clicar sobre el item este se borrara de la lista de los selecciondados
+                    selectedItems.remove(which);
+                }
+            }
+        });
+        alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (selectedItems.size() == 0) {
+                    Toast.makeText(MainActivity.this, "No has seleccionado nada", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    StringBuilder selecciones = new StringBuilder();
+                    for (Integer i : selectedItems) {
+                        selecciones.append(getResources().getStringArray(R.array.colores)[i]).append(" ");
+                    }
+                    Toast.makeText(MainActivity.this, "Has elegido " + selecciones, Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+
+
+        });
+
+        alertDialogBuilder.show();
+    }
+
+
+}

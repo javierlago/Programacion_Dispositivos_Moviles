078bc3ddb778eaa78cb0eb82f5d00c9d493fb906
javierlago <javierlagoamoedo@gmail.com>
2024-02-28 12:05:35 +0100

    wp

--- Persistencia/BasesDeDatosEjemplo/app/src/main/java/com/example/basesdedatosejemplo/MainActivity.java
+++ Persistencia/BasesDeDatosEjemplo/app/src/main/java/com/example/basesdedatosejemplo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.basesdedatosejemplo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteAbortException;
 import android.database.sqlite.SQLiteDatabase;
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.boton_borrar:
                 //Borrado mediante codigo y mediante clausula hardcodeada
+                // En esta operacion no necesitamos el Content Values
                 try{
                     db.execSQL("delete from usuarios where codigo = 3");
                     Toast.makeText(this, "Borrado", Toast.LENGTH_SHORT).show();
@@ -58,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 if(db.delete("usuarios","codigo=2",null)==0){
                     Toast.makeText(this, "Eliminacion erronea", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(this, "no elemento borrado", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Se ha eliminado el elemento", Toast.LENGTH_SHORT).show();
                 }
                 break;
                 
@@ -78,14 +80,69 @@ public class MainActivity extends AppCompatActivity {
                 if (filas==0){
                     Toast.makeText(this, "Caca di vaca en el borrado parametrizado", Toast.LENGTH_SHORT).show();
                 }
+                break;
 
             case R.id.boton_consultar_uno:
-                //todo
+                // Utilizamos codigo SQL directo
+                Cursor cursor = db.rawQuery("select nombre from usuarios where codigo = 1 ",null);
+                if(cursor.moveToFirst()){
+                    String nombre = cursor.getString(0);
+                    Toast.makeText(this, "El nombre recuperado ha sido->"+nombre, Toast.LENGTH_SHORT).show();
 
+                }else {
+                    Toast.makeText(this, "No existe el dato en busqueda directa", Toast.LENGTH_SHORT).show();
+                }
+                cursor.close();
+                // metodo parametrizado
+                String[] datos = {"nombre"};
+                Cursor CursorParametros = db.query("usuarios",datos,"codigo = 4",null,null,null,null);
+                if(CursorParametros.moveToFirst()){
+                    String nombre = CursorParametros.getString(0);
+
+                }else{
+                    Toast.makeText(this, "No existen datos en la busqueda parametrizada", Toast.LENGTH_SHORT).show();
+
+                }
+                CursorParametros.close();
+                break;
             case R.id.boton_consulta_varios:
+                // Utilizamos codigo SQL directo
+                Cursor cursor3 = db.rawQuery("select * from usuarios",null);
+                if(cursor3.moveToFirst()){
+                    do{
+                        // los indices recuperados de la consulta
+                        int codigo = cursor3.getInt(0);
+                        String nombre = cursor3.getString(1);
+                        Toast.makeText(this, "El nombre recuperado ha sido->"+nombre + " Codigo "+ codigo, Toast.LENGTH_SHORT).show();
 
-                //todo
+                    }while (cursor3.moveToNext());
 
+                }else {
+                    Toast.makeText(this, "No existe el dato en busqueda directa", Toast.LENGTH_SHORT).show();
+                }
+                cursor3.close();
+                // metodo parametrizado
+                String[] datos1 = {"codigo","nombre"};
+                Cursor cursor4 = db.query("usuarios",datos1,null,null,null,null,null);
+                if(cursor4.moveToFirst()){
+                    do{
+                    int codigo = cursor4.getInt(0);
+                    String nombre = cursor4.getString(1);
+                    Toast.makeText(this, "Parametrizado-- Nombre-> " + nombre+ " Codigo-> " +codigo, Toast.LENGTH_SHORT).show();
+                    }while (cursor4.moveToNext());
+                }else{
+                    Toast.makeText(this, "No existen datos en la busqueda parametrizada", Toast.LENGTH_SHORT).show();
+
+                }
+                cursor4.close();
+                break;
         }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
     }
 }
